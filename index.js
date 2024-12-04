b1b6ae2a3aa455d006daace69623a9413da77f8f
ulgmaduwantha <u.l.g.maduwantha@gmail.com>
2024-12-04 19:20:23 +0530

    added a body parser middleware

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 const mongoUrl = "mongodb+srv://admin:Ccs151075@cluster0.pcjhx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
+app.use(bodyParser.json())
+
 app.get("/",
 
-    ()=>{
+    (req,res)=>{
+        console.log(req.body)
         console.log("Hello world !")
+        res.json(
+            {
+                "message": "Hi There !",
+            }
+        )
     }
 
 );
@@ -16,8 +25,15 @@ app.get("/",
 app.post("/",
 
 
-    ()=>{
+    (req,res)=>{
+        console.log(req.body)
         console.log("This is a post request")
+        res.json(
+            {
+                "messege": "This is a post reply",
+            }
+        )
+        
     }
 );
 
