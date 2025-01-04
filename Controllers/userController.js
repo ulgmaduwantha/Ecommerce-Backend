5b6c6956b5dcdea35ce26b6e7abea82ecec3d82e
ulgmaduwantha <u.l.g.maduwantha@gmail.com>
2025-01-04 16:47:58 +0530

    create validation function

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -81,4 +81,25 @@ export function loginUser(req, res) {
     });
 }
 
- 
+ export function isAdmin(req){
+    if(req.user == null){
+        return false
+    }
+
+    if(req.user.type != "admin"){
+        return false
+    }
+
+    return true
+ }
+
+ export function isCustomer(req){
+    if(req.user == null){
+        return false
+    }
+
+    if(req.user.type != "customer"){
+        return false
+    }
+    return true
+ }
